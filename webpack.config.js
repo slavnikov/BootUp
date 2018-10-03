9bdc579a4429c96e55d588afd5ac5dcea13c662f
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-10-03 17:15:46 -0400

    Change webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -48,4 +48,9 @@ module.exports = {
     ]
   },
   devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+  ]
 };
