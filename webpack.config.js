672293c28afb9588917040a08da7a142239c7f2e
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-10-04 12:28:19 -0400

    Set mode to production manyally.

--- webpack.config.js
+++ webpack.config.js
@@ -52,5 +52,6 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
     }),
-  ]
+  ],
+  mode: 'production',
 };
