a6d836bf1a418dd867a1836271c588631ce012ba
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-10-26 15:35:17 -0400

    Optimize search query.

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,7 +1,7 @@
 class Api::SearchController < ApplicationController
   def show
     pattern = params[:id]
-    @projects = Project.where("lower (title) like ?", "%#{pattern}%").with_attached_image.where(complete: true).includes('admin', 'backings')
+    @projects = Project.where("title ILIKE ?", "%#{pattern}%").with_attached_image.where(complete: true).includes('admin', 'backings')
 
     render '/api/projects/index.json.jbuilder'
   end
